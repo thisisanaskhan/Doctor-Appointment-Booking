b9d53d4043377a7994409fa74a8f7ba134d9306e
thisisanaskhan <thisisanaskhan7465@gmail.com>
2024-12-26 23:44:33 +0530

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,15 +10,15 @@ const Contact = () => {
       <div className='my-10 flex flex-col md:flex-row gap-10 mb-28 text-sm justify-center'>
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col gap-6  text-gray-700 justify-center items-start'>
-          <p className='text-2xl font-medium'>OUR OFFICE</p>
-          <p>54709 Willms Station <br />Suite 350, Washington,USA</p>
+          <p className='text-lg text-gray-600 font-medium'>OUR OFFICE</p>
+          <p className='text-gray-500'>54709 Willms Station <br />Suite 350, Washington,USA</p>
         
-          <p>Tel:(415) 555-0132 <br />Email:greatstackdev@gmail.com</p>
+          <p className='text-gray-500'>Tel:(415) 555-0132 <br />Email:greatstackdev@gmail.com</p>
           
-          <p>CAREERS AT PRESCRIPTO</p>
-          <p>Learn more about our teams and job openings.</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
 
-          <button className='border border-gray-400 p-3'>Explore Jobs</button>
+          <button className='border border-gray-400 p-3 hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
       </div>
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state,setState]=useState('Sign Up')
+
+  const[email,setEmail]=useState('')
+  const[pass,setPass]=useState('')
+  const[name,setName]=useState('')
+
+  const onSubmitHandler=async(event)=>{
+    event.preventDefault()
+  }
   return (
-    <div></div>
+    <form className='min-h-[80vh] flex items-center '>
+      <div className='flex flex-col items-start m-auto gap-3 p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-sm text-zinc-600 shadow-lg'>
+      <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account":"Login"}</p>
+      <p>Please {state === 'Sign Up' ? "sign up":"login"} to book appointment</p>
+      {
+        state === "Sign Up" && 
+        <div className='w-full'>
+          <p>Full Name</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text"  onChange={(e)=>setName(e.target.name)} value={name}/>
+
+        </div>
+      }
+       
+       <div className='w-full '>
+       <p>E-mail</p>
+       <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email"  onChange={(e)=>setEmail(e.target.name)} value={email}/>
+       </div>
+       <div className='w-full '>
+       <p>Password</p>
+       <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password"  onChange={(e)=>setPass(e.target.name)} value={pass}/>
+       </div>
+
+       <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account":"Login"}</button>
+       {
+        state === "Sign Up"
+        ? <p>Already have an account?<span onClick={()=>{setState('Log In')}} className='text-blue-400 underline cursor-pointer'> Login here</span></p>
+        : <p>Create an new account?<span onClick={()=>{setState('Sign Up')}}  className='text-blue-400 underline cursor-pointer'>Click here</span></p>
+       }
+      </div>
+
+    </form>
   )
 }
 
